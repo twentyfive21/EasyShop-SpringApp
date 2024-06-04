d7da77305c33b9fdb3664074c5206f3b90fd2913
twentyfive21 <tina.winnn@gmail.com>
2024-06-03 23:04:06 -0400

    added admin code

--- capstone-starter/src/main/java/org/yearup/EasyshopApplication.java
+++ capstone-starter/src/main/java/org/yearup/EasyshopApplication.java
@@ -12,3 +12,5 @@ public class EasyshopApplication
     }
 
 }
+// admin token
+// eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOIiwiZXhwIjoxNzE3NTcyMzEwfQ.8ZcKtJ_bVmBScH9to2n0kcNjbv82-jOn_T8WoFH8RTy08dWxpPA_vHAFjiUxzGAiNbTxaS7IyVLC4iuYbR9yMg
