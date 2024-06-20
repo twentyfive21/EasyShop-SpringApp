592fb116cee9f5c9a9867a4d85c9e17dccada9a4
twentyfive21 <tina.winnn@gmail.com>
2024-06-20 11:56:26 -0400

    try catch added for error in profile

--- capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,8 +1,10 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProfileDao;
 import org.yearup.data.UserDao;
 import org.yearup.models.Profile;
@@ -18,26 +20,36 @@ public class ProfileController {
 
     private ProfileDao profileDao;
     private UserDao userDao;
-
-    //  create an Autowired controller to inject the profileDao
     @Autowired
-    public ProfileController(UserDao userDao, ProfileDao profileDao) {
-        this.userDao = userDao;
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
         this.profileDao = profileDao;
+        this.userDao = userDao;
     }
 
     @GetMapping()
    public Profile getProfileById(Principal principal){
-    String userName = principal.getName();
-    User user = userDao.getByUserName(userName);
-    return profileDao.getProfileById(user.getId());
+   try{
+       String userName = principal.getName();
+       User user = userDao.getByUserName(userName);
+       var profile = profileDao.getProfileById(user.getId());
+       if(profile == null)
+           throw  new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+       return profile;
+   }catch (Exception e){
+       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Error Loading Profile");
+   }
    }
 
    @PutMapping()
     public void update(@RequestBody Profile profile, Principal principal){
-       String userName = principal.getName();
-       User user = userDao.getByUserName(userName);
-       profileDao.update(user.getId(),profile);
+      try{
+          String userName = principal.getName();
+          User user = userDao.getByUserName(userName);
+          profileDao.update(user.getId(),profile);
+      }catch (Exception e){
+          throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Profile not able to update");
+      }
     }
 
 
